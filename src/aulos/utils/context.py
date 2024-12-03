5b292305bead7521382988b1f7296099b7bfd926
Oujox <Oujoxyz365@outlook.jp>
2024-12-03 20:30:19 +0900

    context 微修正

--- src/aulos/utils/context.py
+++ src/aulos/utils/context.py
@@ -1,9 +1,9 @@
-import pathlib
-import tomllib
 import typing as t
+import json
+import tomllib
+from pathlib import Path
 from contextlib import ContextDecorator
 from contextvars import ContextVar
-
 from dacite import from_dict
 
 from .._core import Object, Setting
@@ -15,17 +15,27 @@ class Context(ContextDecorator, Object):
     internal: t.Final[ContextVar[Setting]] = ContextVar("internal")
 
     def __init__(self, setting: Setting) -> None:
-        self.__token = self.internal.set(setting)
+        self._token = self.internal.set(setting)
         super().__init__(setting)
 
     def __enter__(self) -> t.Self:
         return self
 
     def __exit__(self, *tracebacks):
-        self.internal.reset(self.__token)
+        self.internal.reset(self._token)
 
     @classmethod
-    def from_toml(cls, path: pathlib.Path) -> t.Self:
+    def setting(self) -> t.Optional[Setting]:
+        return self.internal.get(None)
+
+    @classmethod
+    def from_toml(cls, path: Path) -> t.Self:
         setting = tomllib.load(open(path, mode="rb"))
+        setting = from_dict(Setting, setting)
+        return cls(setting)
+
+    @classmethod
+    def from_json(cls, path: Path) -> t.Self:
+        setting = json.load(open(path, mode="rb"))
         setting = from_dict(Setting, setting)
         return cls(setting)
