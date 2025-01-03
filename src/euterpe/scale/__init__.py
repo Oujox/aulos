4bff4e8613a8385711c1e752d3b7bbb31766ea4b
oujox <Oujoxyz365@outlook.jp>
2025-01-03 23:35:31 +0900

    fix

--- src/euterpe/scale/__init__.py
+++ src/euterpe/scale/__init__.py
@@ -4,11 +4,6 @@ This module provides a collection of music theory tools for working with scales 
 It includes diatonic and nondiatonic scales, as well as several modes derived from both the 
 major, harmonic minor, and melodic minor scales. These scales and modes are useful for 
 composition, music analysis, and algorithmic music generation.
-
-Modules:
-    - DiatonicScale, NondiatonicScale: Base classes for diatonic and nondiatonic scales.
-    - Modes
-    - Scales
 """
 
 from .diatonic import DiatonicScale, NondiatonicScale
--- src/euterpe/tuner/__init__.py
+++ src/euterpe/tuner/__init__.py
@@ -1,6 +1,9 @@
-"""Tuner
+""" Tuner
 ---
-* 一般に使用される音律を提供
+This module provides a collection of music theory tools for working with tuning systems.
+It includes various historical and contemporary tuners, offering precise control over pitch 
+relationships in different temperaments. These tuners are useful for performance, 
+composition, and music analysis.
 """
 
 from .implements.tuner import (Equal12Tuner, JustIntonationTuner,
