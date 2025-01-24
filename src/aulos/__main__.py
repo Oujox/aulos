8003ec994e4af1f6c1ccb8fa0644e62b05240ee4
oujox <Oujoxyz365@outlook.jp>
2025-01-25 05:27:08 +0900

    be called from scripts

--- src/aulos/__main__.py
+++ src/aulos/__main__.py
@@ -1,18 +1,34 @@
-import argparse
+def run_gui():
+    import ctypes
+    import tkinter as tk
+    from aulos.ui import ScaleViewer, KeyBoard
+
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+    root = tk.Tk()
+    root.title("Aulos Application GUI")
+    root.geometry("1200x800")
+    root.resizable(False, False)
+    root.attributes('-topmost', True)
+    keyboard = KeyBoard(root)
+    keyboard.pack(fill=tk.X)
+    scale = ScaleViewer(root)
+    scale.pack(fill=tk.X)
+
+    root.mainloop()
 
 
 def main():
+    import argparse
+
     parser = argparse.ArgumentParser(description="Aulos Application")
     parser.add_argument(
-        "--gui", action="store_true", help="run the aulos application in GUI mode"
+        "-gui", action="store_true", help="run the aulos application in GUI mode"
     )
     args = parser.parse_args()
 
     if args.gui:
-        from aulos.ui import run
-
-        run()
-
+        run_gui()
 
 if __name__ == "__main__":
     main()
